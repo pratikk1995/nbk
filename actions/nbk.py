439a1f739e2fa61ab15def716edb7e18f7c134b0
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-26 18:10:41 +0530

    Update nbk.py

--- actions/nbk.py
+++ actions/nbk.py
@@ -3,7 +3,7 @@ import json
 import requests
 from st2common.runners.base_action import Action
 class Myclass:
-    def run(self,id,title):
+    def run(self,id,title,timeout):
       try:
             x = {"ID": id, "TITLE": title}
             x1= json.dumps(x)
