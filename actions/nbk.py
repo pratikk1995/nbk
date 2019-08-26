137bdfb0df632324443e6dac10e2b7d912847c31
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-26 17:26:43 +0530

    Update nbk.py

--- actions/nbk.py
+++ actions/nbk.py
@@ -1,15 +1,14 @@
 import sys
 import requests
 from st2common.runners.base_action import Action
-
-  class Myclass:
+class Myclass:
     def run(self, id, title)
-          try:
+      try:
             x = { "Id": id,"title": title}
             x1=json.dumps(x)
             r = requests.post('https://fakerestapi.azurewebsites.net/api/Books',x = x1)
             print(r)
-          except requests.exceptions.Timeout:
+      except requests.exceptions.Timeout:
             print("timeout")
             sys.exit(0)
     
