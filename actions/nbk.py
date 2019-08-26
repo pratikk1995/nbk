6806ad83d98f0b4b1d6e564a564ee7c68c6bc71d
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-26 17:52:15 +0530

    Update nbk.py

--- actions/nbk.py
+++ actions/nbk.py
@@ -5,7 +5,7 @@ class Myclass:
     def run(self,id,title):
       try:
             x = {"ID": id, "TITLE": title}
-            x1=json.dumps(x)
+            x1= json.dumps(x)
             url='https://fakerestapi.azurewebsites.net/api/Books'
             header='Content-Type: application/json'
             r = requests.post(url, header=header, data = x1)
