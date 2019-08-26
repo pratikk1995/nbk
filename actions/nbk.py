7fe0d92d107fb3e85af4d965c34da09a18ae725c
piku1595 <pratik.khairnar1995@gmail.com>
2019-08-26 18:22:17 +0530

    Update nbk.py

--- actions/nbk.py
+++ actions/nbk.py
@@ -3,12 +3,12 @@ import json
 import requests
 from st2common.runners.base_action import Action
 class Myclass(Action):
-    def run(self,id,title,desc,pgc,exc,pbp):
+    def run(self,id,title,descp,pgcount,excerpt,pubdate):
       try:
-            x = {"ID": id, "TITLE": title, "Description": desc, "PageCount":pgc, "Excerpt": exc, "PublishDate": pbd }
+            x ={"ID":id,"Title":title,"Description": descp,"PageCount": pgcount,"Excerpt": excerpt,  "PublishDate": pubdate}
             x1= json.dumps(x)
             url='https://fakerestapi.azurewebsites.net/api/Books'
-            headers='Content-Type: application/json'
+            headers={'Content-Type: application/json'}
             r = requests.post(url, headers= headers, data = x1,timeout=6.0)
             y= r.json()
             print(r)
